c2347ef58c9aaf86e9405cc7c0ed065f6b9510bb
Sebastian Valdez <a20191970@pucp.edu.pe>
2024-07-03 09:58:57 +0000

    fixed

--- deploy.sh
+++ deploy.sh
@@ -20,6 +20,7 @@ echo "The parameter provided is: $PARAMETER"
 # Example of using the parameter in a command
 sed -i "s|http://localhost:3000|$PARAMETER/api|g" IA-Angel/app/model/model.py
 
+cd IA-Angel/app/
 python3 -m venv env
 source env/bin/activate
 pip install -r requirements.txt
